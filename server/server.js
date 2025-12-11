752664701a6ec33aec344e447b11fdf00621e74f
ERIK J CLARK <ERIKJCLARK@ICLOUD.COM>
2025-12-11 18:42:57 +0000

    build: rm console.log()s

--- server/server.js
+++ server/server.js
@@ -10,25 +10,17 @@ app.use(express.json());
 
 const PORT = 8080;
 app.listen(PORT, () => console.log(`This app is running on port ${PORT}`));
-<<<<<<< HEAD
-=======
-
-console.log(`success 3, probably`);
->>>>>>> 29854b8768f4623e7837d50570a4ceb02416fceb
 
 app.post("/message", (req, res) => {
     const newMessage = req.body.userEntry;
-    console.log(newMessage);
     const query = db.query(
         `INSERT INTO messageboard (name, message) VALUES ($1, $2)`,
         [newMessage.name, newMessage.message]
     );
     res.json({status: "success", values: newMessage});
-    console.log(`Success 4, probably`);
 });
 
 app.get('/readMessages', async (req, res) => {
     const query = await db.query(`SELECT name, message FROM messageboard;`);
-    console.log(query);
     res.json(query.rows);
 })
