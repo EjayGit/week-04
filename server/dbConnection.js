e0542a379082750d28960761592f255f726e73b6
ERIK J CLARK <ERIKJCLARK@ICLOUD.COM>
2025-12-13 23:26:35 +0000

    fix: add create table if not exists to dbConnection.js

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -5,3 +5,10 @@ dotenv.config();
 export const db = new pg.Pool({
     connectionString: process.env.DB_CONN,
 });
+
+db.query(
+`CREATE TABLE IF NOT EXISTS messageboard(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255),
+    message TEXT
+)`);
