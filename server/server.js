749d4c33d5dbb4a4717c5bd516a29abff76ec60f
ERIK J CLARK <ERIKJCLARK@ICLOUD.COM>
2026-01-04 16:34:02 +0000

    like button created, but needs transforming into either a like shaped button, or to have a 'like' word placed next to it.

--- server/server.js
+++ server/server.js
@@ -11,12 +11,26 @@ app.use(express.json());
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`This app is running on port ${PORT}`));
 
+app.post("/likes", async (req, res) => {
+    try{
+        const likeData = req.body.likeData;
+        console.log(likeData.id);
+        console.log(likeData.value);
+        const query = await db.query(
+            'UPDATE messageboard SET likes = likes + ($1) WHERE id = ($2)',
+            [likeData.value, likeData.id]
+        );
+    } catch(error){
+        res.status(500).json({Error: error.message});
+    }
+});
+
 app.post("/message", async (req, res) => {
     try{
         const newMessage = req.body.userEntry;
         const query = await db.query(
-            `INSERT INTO messageboard (name, message) VALUES ($1, $2)`,
-            [newMessage.name, newMessage.message]
+            `INSERT INTO messageboard (name, message, likes) VALUES ($1, $2, $3)`,
+            [newMessage.name, newMessage.message, 0]
         );
         res.json({status: "success", values: newMessage});
     } catch(error){
@@ -26,7 +40,7 @@ app.post("/message", async (req, res) => {
 
 app.get('/readMessages', async (req, res) => {
     try{
-        const query = await db.query(`SELECT id, name, message FROM messageboard;`);
+        const query = await db.query(`SELECT id, name, message, likes FROM messageboard;`);
         res.json(query.rows);
     } catch(error){
         console.log("db error: ", error);
